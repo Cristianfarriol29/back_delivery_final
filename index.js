ce30fc83d61d2aa53565199a52aab806c070f838
cristian <cristianfarriol19@hotmail.com>
2022-05-17 16:45:28 +0200

    act

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ configCloudinary();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin: *");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
